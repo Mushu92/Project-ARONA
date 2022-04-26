b1f82b009b90c5830bd28dad86a8d5008e7f11d0
DESKTOP-KDPTOSJ\mind9 <mind9873@naver.com>
2022-04-26 23:31:57 +0900

    카메라 이동

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    //캐릭터 위치 정보
+    [SerializeField]
+    Transform playerTransform;
+    //카메라 위치 정보
+    [SerializeField]
+    Vector3 cameraPosition;
+
+    //맵의 중앙
+    [SerializeField]
+    Vector2 center;
+    //맵의 크기
+    private Vector2 mapSize;
+
+    //카메라 이동 속도
+    [SerializeField]
+    float cameraMoveSpeed;
+
+    private float height;
+    private float width;
+
+    void Start()
+    {
+        //플레이어 위치 정보 가져옴
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        
+        Transform mapSizeTransform = GameObject.Find("World").GetComponent<Transform>();
+        mapSize = new Vector2(mapSizeTransform.localScale.x /2 , mapSizeTransform.localScale.y / 2);
+
+        //카메라 범위 계산
+        height = Camera.main.orthographicSize;
+        width = height * Screen.width / Screen.height;
+    }
+
+    void FixedUpdate()
+    {
+        LimitCameraArea();
+    }
+
+    //영역(Map) 밖을 비추지 않도록 영역 제한
+    void LimitCameraArea()
+    {
+        //선형 보간 사용해서 카메라 이동 부드럽게
+        transform.position = Vector3.Lerp(transform.position, 
+                                          playerTransform.position + cameraPosition, 
+                                          Time.deltaTime * cameraMoveSpeed);
+        
+        
+        //Clamp 이용해서 범위 제한
+        float lx = mapSize.x - width;
+        float clampX = Mathf.Clamp(transform.position.x, -lx + center.x, lx + center.x);
+
+        float ly = mapSize.y - height;
+        float clampY = Mathf.Clamp(transform.position.y, -ly + center.y, ly + center.y);
+
+        transform.position = new Vector3(clampX, clampY, -10f);
+    }
+}
