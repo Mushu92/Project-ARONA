f055ce2bfcc6b170c17706b0ab523c6021bd577e
DESKTOP-QRVKDEO\YEG <euginyun@gmail.com>
2022-05-07 09:34:14 +0900

    Player Motion State Update

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,29 +6,36 @@ public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
     Rigidbody2D rigid;
-
+    Animator animator;
     Vector3 playerSize;
 
     void Awake() {
         rigid = GetComponent<Rigidbody2D>();
         playerSize = GetComponent<Renderer>().bounds.size;
-    } 
+        animator = GetComponent<Animator>();
+    }
 
-    private void Update() {
+    private void Update()
+    {
         var distance = (transform.position - Camera.main.transform.position).z;
- 
-         var leftBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance)).x + (playerSize.x/2);
-         var rightBorder = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance)).x - (playerSize.x/2);
- 
-         var bottomBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance)).y + (playerSize.y/2);
-         var topBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, distance)).y - (playerSize.y/2);
-     
-         // Here the position of the player is clamped into the boundaries
-         transform.position = (new Vector3 (
-             Mathf.Clamp (transform.position.x, leftBorder, rightBorder),
-             Mathf.Clamp (transform.position.y, bottomBorder, topBorder),
-             transform.position.z)
-        );
+
+        var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).x + (playerSize.x / 2);
+        var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance)).x - (playerSize.x / 2);
+
+        var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)).y + (playerSize.y / 2);
+        var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distance)).y - (playerSize.y / 2);
+
+        // Here the position of the player is clamped into the boundaries
+        transform.position = (new Vector3(
+            Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+            Mathf.Clamp(transform.position.y, bottomBorder, topBorder),
+            transform.position.z)
+       );
+
+        if (rigid.velocity.normalized.x == 0)
+            animator.SetBool("isWalking", false);
+        else
+            animator.SetBool("isWalking", true);
     }
 
     void FixedUpdate()
