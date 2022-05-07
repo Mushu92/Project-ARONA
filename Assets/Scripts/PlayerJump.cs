7d7fcd70a1f765203d9b815c037775e298c71f75
DESKTOP-QRVKDEO\YEG <euginyun@gmail.com>
2022-05-07 11:24:15 +0900

    Infinite jump bug update (Incomplete)

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -24,4 +24,17 @@ public class PlayerJump : MonoBehaviour
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
     }
+   /* void FixedUdate()
+    {
+        //Landing Platform
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if(rayHit.collider != null)
+        {
+            if(rayHit.distance < 0.5f )
+                Debug.Log(rayHit.collider.name);
+        }
+    } */
 }
