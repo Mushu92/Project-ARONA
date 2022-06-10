20adab2373b42a5be871804fd66a6002cdd39ea1
DESKTOP-83AK1LK\PCH <pch2395@gmail.com>
2022-06-10 22:21:26 +0900

    타이틀 화면 작업 중

--- Assets/Scripts/TitleManagerAction.cs
+++ Assets/Scripts/TitleManagerAction.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleManagerAction : MonoBehaviour
 {
+    Image fadeIn;
+    int startBtn;
+    private Animator fadeIO;
     // Start is called before the first frame update
-    void Start()
+    void FadeInOut()//타이틀화면 진입시 페이드아웃 위한 세팅
     {
-        
+        GameObject fadeInFind = GameObject.Find("FadeIn");
+        fadeIO = GameObject.Find("FadeIn").GetComponent<Animator>();
+        fadeIn = fadeInFind.GetComponent<Image>();
     }
+
+    private void Start()
+    {
+        FadeInOut();
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
 }
